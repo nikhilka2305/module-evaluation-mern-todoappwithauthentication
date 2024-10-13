e04ec6ae0be6aeafa7089de0b9952a6f29edc9c0
nikhilka <nikhilka23@gmail.com>
2024-10-13 20:54:39 +0530

    Some correction

--- frontend/src/AddToDo.jsx
+++ frontend/src/AddToDo.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { UserContext } from "./userLoginContext";
+import { UserContext } from "./UserLoginContext";
 import { NavLink, useNavigate } from "react-router-dom";
 export default function AddToDo() {
 	const navigate = useNavigate();
